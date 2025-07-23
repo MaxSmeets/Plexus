b327115813d52a2ed934b1501dfc72b964a26992
MaxSmeets <maxsmeets1996@gmail.com>
2025-07-23 13:09:12 +0200

    REMOVE: Test MCP integration

--- server/agents/user_agent.py
+++ server/agents/user_agent.py
@@ -1,26 +1,24 @@
 import asyncio 
 from agno.agent import Agent
 from agno.models.ollama import Ollama
-from agno.tools.mcp import MCPTools
 
 async def user_agent():
-    async with MCPTools(command="uvx mcp-server-git") as mcp_tools:
-        agent = Agent(
-            model=Ollama("llama3.2:latest"),
-            tools=[mcp_tools],
-            instructions="You can use your tools to make git commits and push changes.",
-            markdown=True,
-            monitoring=True,
-            stream=True,
-            reasoning=True
-        )
+    agent = Agent(
+        model=Ollama("llama3.2:latest"),
+        tools=[],
+        instructions="You are a helpful assistant called Ada. Answer the user's questions to the best of your ability.",
+        markdown=True,
+        monitoring=True,
+        stream=True,
+        reasoning=False
+    )
 
-        while True:
-            user_input = input("You: ")
-            if user_input.lower() in ["exit", "quit"]:
-                print("Exiting the chat.")
-                break
-            await agent.aprint_response(user_input)
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            print("Exiting the chat.")
+            break
+        await agent.aprint_response(user_input)
 
 if __name__ == "__main__":
     asyncio.run(user_agent())
